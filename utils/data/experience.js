e81b7215e0f2ea4cb1566e0c6b0c96653dd82943
Aniruddh Rao <aniruddhrao03@gmail.com>
2024-04-03 12:36:10 -0400

    fixed expereinces

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,26 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Site Reliability Engineer Intern',
-    company: "Royal Bank of Canada",
-    duration: "(May 2023 – Aug 2023)"
+    title: 'Software Engineer Intern',
+    company: "Bank of Canada",
+    duration: "Jan 2024 – Apr 2024"
   },
   {
     id: 2,
-    title: "Automation Test Engineer Intern",
-    company: "OpenText",
-    duration: "(Sep 2022 - Dec 2022)"
+    title: 'Site Reliability Engineer Intern',
+    company: "Royal Bank of Canada",
+    duration: "May 2023 – Aug 2023"
   },
   {
     id: 3,
-    title: "Software Engineering Intern",
+    title: "Automation Test Engineer Intern",
+    company: "OpenText",
+    duration: "Sep 2022 - Dec 2022"
+  },
+  {
+    id: 4,
+    title: "Software Engineer Intern",
     company: "Bell and Howell",
-    duration: "(Jan 2022 - Apr 2022)"
+    duration: "Jan 2022 - Apr 2022"
   }
 ]
