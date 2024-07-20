715b43deb70bbab36d98641b4677b4e338a11b86
Aniruddh Rao <aniruddhrao03@gmail.com>
2024-07-20 01:12:21 -0400

    changed exp

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,25 +1,25 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer Intern',
+    title: 'Software and Controls Engineering Intern',
     company: "Bank of Canada",
     duration: "Jan 2024 – Apr 2024"
   },
   {
     id: 2,
-    title: 'Site Reliability Engineer Intern',
+    title: 'Site Reliability Engineering Intern',
     company: "Royal Bank of Canada",
     duration: "May 2023 – Aug 2023"
   },
   {
     id: 3,
-    title: "Automation Test Engineer Intern",
+    title: "Automation Test Engineering Intern",
     company: "OpenText",
     duration: "Sep 2022 - Dec 2022"
   },
   {
     id: 4,
-    title: "Software Engineer Intern",
+    title: "System Design Engineering Intern",
     company: "Bell and Howell",
     duration: "Jan 2022 - Apr 2022"
   }
