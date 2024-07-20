bad3c7829f05368b9c3f38460ad9cbb4d48da45e
Aniruddh Rao <aniruddhrao03@gmail.com>
2024-07-20 01:17:07 -0400

    changed desc

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Aniruddh Rao",
   designation: "Student",
-  description: "And a passionate student developer from Toronto 🍁",
+  description: "And a passionate engineering student from Toronto 🍁",
   email: 'a36rao@uwaterloo.ca',
   github: 'https://github.com/aniruddhrao03',
   linkedIn: 'https://www.linkedin.com/in/aniruddhrao03/',
